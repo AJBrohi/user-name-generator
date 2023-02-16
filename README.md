f656d7d73ebfdaf4e3535a4f52f3dc40313ab4f7
AJBrohi <brohi96@gmail.com>
2023-02-16 22:07:06 +0600

    user name generator

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # User Name Generator
+
+[User Name Generator](https://imgur.com/a/23Ero07)
---
+++ main.py
@@ -0,0 +1,9 @@
+print("Welcome To The User Name Generator by AJBrohi!\n")
+
+name = input("Please Enter Your Nick Name or Last Name: ")
+digit = input("Please Enter Last Two Digits of Your Birth Year: ")
+
+username = name + str(digit)
+
+print("Generating A Username For You...")
+print("Your Username Is: "+ username+"\n")
